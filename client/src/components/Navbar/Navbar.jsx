90f4f81960561e5b3e16e3e86b60c9e9975679ec
Aidan Tilgner <aidantilgner02@gmail.com>
2021-04-30 19:03:56 -0700

    built most of navbar and added style

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import './Navbar.scss';
+import { Link } from 'react-router-dom';
 
-function Navbar() {
+function Navbar(props){
     return (
-        <div>
-            
+        <div className="navbar">
+            <h3 className="navbar__logo">SOAPBOX</h3>
+            <ul className="navbar__items">
+                <li className="navbar__item">
+                    Search
+                </li>
+                <li 
+                    className={`navbar__item ${props.active === 'home' && "active"} tablet`}
+                >
+                    <Link to="/Home" className="navbar__link"/>Home<Link/>
+                </li>
+                <li className={`navbar__item ${props.active === 'topics' && "active"} tablet`}>
+                    <Link to="/Topics" className="navbar__link">Topics</Link>
+                </li>
+                <li className={`navbar__item ${props.active === 'upload' && "active"} tablet`}>
+                    <Link to="/Upload" className="navbar__link">Upload</Link>
+                </li>
+                <li className={`navbar__item ${props.active === 'user profile' && "active"} tablet`}>
+                    <Link to={`/User/${props.username ? props.username : 'Soapbox'}`} className="navbar__link">User Profile</Link>
+                </li>
+            </ul>
         </div>
     )
 }
