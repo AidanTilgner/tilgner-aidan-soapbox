3180f4d4285c133a99d18b4540bd31b3763d720b
Aidan Tilgner <aidantilgner02@gmail.com>
2021-04-30 21:20:43 -0700

    started on basic stucture of backend

--- client/src/App.js
+++ client/src/App.js
@@ -16,45 +16,76 @@ import UserProfile from './pages/UserProfile/UserProfile';
 //components
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-        <Switch>
-          <Redirect exact from='/' to='/home'/>
-          <Route path='/home'
-            render={props => {
-              <Home {...props}/>
-            }}
-          />
-          <Route path="/search/:query"
-            render={props => {
-              <Search {...props}/>
-            }}
-          />
-          <Route path="/essays/:id"
-            render={props => {
-              <EssayView {...props}/>
-            }}
-          />
-          <Route path="/topics"
-            render={props => {
-              <Topics {...props}/>
-            }}
-          />
-          <Route path="/upload"
-            render={<Upload/>}
-          />
-          <Route path="/user/:username"
-            render={props => {
-              <UserProfile {...props}/>
-            }}
-          />
-        </Switch>
-      <Footer/>
-    </div>
-  );
+//axios stuff
+const API_PORT = 8080;
+const API_URL = `http://localhost:${API_PORT}`;
+
+class App extends React.Component {
+
+  state={
+    recommendedEssays: [],
+    recommendedTopics: []
+  }
+
+  componentDidMount = () => {
+    axios.get(API_URL + '/essays')
+    .then(res => {
+      // this.setState({
+      //   recommendedEssays: res,
+      // })
+      console.log(res)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Navbar/>
+          <Switch>
+            <Redirect exact from='/' to='/home'/>
+            <Route path='/home'
+              render={props => {
+                <Home 
+                  {...props}
+                  recommendedVideos={this.state.recommendedVideos}
+                  recommendedTopics={this.state.recommendedTopics}
+                />
+              }}
+            />
+            <Route path="/search/:query"
+              render={props => {
+                <Search {...props}/>
+              }}
+            />
+            <Route path="/essays/:id"
+              render={props => {
+                <EssayView {...props}/>
+              }}
+            />
+            <Route path="/topics"
+              render={props => {
+                <Topics {...props}/>
+              }}
+            />
+            <Route path="/upload"
+              render={<Upload/>}
+            />
+            <Route path="/user/:username"
+              render={props => {
+                <UserProfile {...props}/>
+              }}
+            />
+          </Switch>
+        <Footer/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
+import axios from 'axios';
 
-function Home(props) {
-    let screenSize = window.screen.width;
-    return (
-        <div className="home">
-            <Navbar active="home" screenSize={screenSize}/>
-        </div>
-    )
+class Home extends React.Component {
+
+    state={
+        recommendedEssays: this.props.recommendedEssays,
+        recommendedTopics: this.props.recommendedTopics,
+    }
+
+    render(){
+        return (
+            <div className="home">
+                <Navbar active="home"/>
+            </div>
+        )
+    }
 }
 
 export default Home
